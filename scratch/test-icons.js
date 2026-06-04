182daef5b7cc2c2b1c30b020ca1d4425aa6941e7
Meet Ukani <meetukani34-prog@users.noreply.github.com>
2026-06-05 01:16:10 +0530

    Remove unwanted scratch and temp files

--- scratch/test-icons.js
+++
@@ -1,6 +0,0 @@
-import { VscAzure, VscVscode } from "react-icons/vsc";
-import { FaAws } from "react-icons/fa";
-import { DiPhotoshop } from "react-icons/di";
-import { TbBrandVscode } from "react-icons/tb";
-
-console.log("Imports successful");
